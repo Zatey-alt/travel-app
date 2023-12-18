896ca5dc58e5e93c5a75f91712e096e2dbf60af3
Zatey <michael.zatey@kibo.school>
2023-12-18 14:47:19 +0000

    add axios rollup

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/react-fontawesome'],
+      external: ['@fortawesome/react-fontawesome','axios'],
     },
   },
 })
